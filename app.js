8cadf253c2d0384c39c649a8d3902bcc82b6d640
Maurício Meira <mauriciodealmeidamf@gmail.com>
2021-10-14 08:43:31 -0300

    feat: main calls and methods

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.status(200).json({
-    message: 'Tudo certo!',
+    message: 'API em funcionamento!',
   });
 });
 
 app.listen(3030, () => {
   console.log('API Iniciada - Porta 3030');
-})
+});
