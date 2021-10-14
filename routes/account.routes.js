5af5a1abc09d66a6c570d11a68b6e2e99f793147
Maurício Meira <mauriciodealmeidamf@gmail.com>
2021-10-14 09:36:36 -0300

    feat(accounts): get by id

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const accountsMock = require('../mocks/accounts');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Dados de conta!',
-  });
+router.get('/:id', (req, res) => {
+  const account = accountsMock.find(
+    (acc) => acc.account_number === req.params.id
+  );
+
+  if (!account) {
+    res.status(404).json({
+      message: `Não foi encontrada uma conta com este número: ${req.params.id}`,
+    });
+  }
+
+  res.status(200).json(account);
 });
 
 router.get('/balance', (req, res) => {
